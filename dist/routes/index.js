f9bd66de7a8b0a1d7a7554a535501d2e730ca95b
sabiss <sabrinabezsilva@gmail.com>
2023-09-24 18:02:39 -0300

    feat: nova rota para o app

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,10 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const usuariosRouter_js_1 = __importDefault(require("../routes/usuariosRouter.js"));
 const routes = (app) => {
     //pega a instância do express que é o app
     app.use(
     //outras rotas que serão USE(u)das
-    express_1.default.json());
+    express_1.default.json(), usuariosRouter_js_1.default);
 };
 exports.default = routes;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import usuarios from "../routes/usuariosRouter.js";
 
 const routes = (app) => {
   //pega a instância do express que é o app
   app.use(
     //outras rotas que serão USE(u)das
-    express.json()
+    express.json(),
+    usuarios
   );
 };
 
