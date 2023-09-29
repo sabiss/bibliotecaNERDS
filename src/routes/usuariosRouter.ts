46b0200cabdd510320e9687da03132198bd0e621
sabiss <sabrinabezsilva@gmail.com>
2023-09-29 18:05:46 -0300

    adicionando o middleware para todas as rotas para testar sua funcionalidade

--- src/routes/usuariosRouter.ts
+++ src/routes/usuariosRouter.ts
@@ -1,9 +1,11 @@
 import usuariosController from "../controllers/usuariosController";
+import middleware from "../middleware/authMiddleware";
 import express from "express";
 
 const router = express.Router();
 
 router
+  .use(middleware)
   .get("/usuarios", usuariosController.listarUsuarios)
   .post("/usuarios", usuariosController.cadastrarUsuario)
   .delete("/usuarios/:id", usuariosController.deletarUsuario)
