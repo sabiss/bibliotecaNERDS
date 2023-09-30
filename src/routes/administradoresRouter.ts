950bdd3916c158fd45eda925707692ed0becab07
sabiss <sabrinabezsilva@gmail.com>
2023-09-30 20:51:50 -0300

    FEAT: adicionando rota de cadastro de administradores

--- src/routes/administradoresRouter.ts
+++ src/routes/administradoresRouter.ts
@@ -5,10 +5,10 @@ import administradoresController from "../controllers/administradoresController"
 const router = express.Router();
 
 router
-  .post("/adm/cadastroAdm", administradoresController.cadastrarAdministrador)
   .post("/adm/login", administradoresController.logarNoSistema) //irei pensar numa lógica melhor
 
   .use(middleware) //rotas que precisam de autenticação para serem acessadas
+  .post("/adm/cadastroAdm", administradoresController.cadastrarAdministrador)
   .get("/adm/listarUsers", administradoresController.listarUsuarios)
   .post("/adm/cadastroUser", administradoresController.cadastrarUsuario)
   .delete("/adm/deletarUser/:id", administradoresController.deletarUsuario);
