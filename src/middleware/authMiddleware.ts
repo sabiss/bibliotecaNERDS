09a0c8a867dd5aee6749db3abbffdc45ddd11d5c
sabiss <sabrinabezsilva@gmail.com>
2023-09-30 06:54:24 -0300

    FEAT: comantando trechos e melhorando responses

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,18 +2,24 @@ import express from "express";
 import jwt from "jsonwebtoken";
 
 export default async (req, res, next) => {
-  const auth = req.headers.authorization;
+  const auth = req.headers.authorization; //pegando a autorização no header da requisição
 
   if (!auth) {
-    return res.status(401).send({ message: "Requisição sem token" });
+    //não tem autorização
+    return res
+      .status(401)
+      .send({ message: "Requisição sem token. Faça Login" });
   }
 
   const [, token] = auth.split(" ");
   try {
     const payload = jwt.verify(`${token}`, `${process.env.APP_SECRET}`);
   } catch (err) {
-    return res.status(401).send({ message: "token inválido" });
+    //tem token, mas ele está errado ou expirado
+    return res
+      .status(401)
+      .send({ message: "Token inválido ou expirado. Faça login no sistema" });
   }
 
-  return next();
+  return next(); //deu tudo certo, pode continuar o processo
 };
