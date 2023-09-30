d295c5b3b9429e8c0b243569f0bf4be8e0d14243
sabiss <sabrinabezsilva@gmail.com>
2023-09-30 19:00:02 -0300

    FIX: consertando erro de identificação do payload.id

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 
+interface JwtPayload {
+  //faz a comando "req.idUserRequest = payload.id" funcionar
+  id: string;
+}
+
 export default async (req, res, next) => {
   const auth = req.headers.authorization; //pegando a autorização no header da requisição
 
@@ -13,7 +18,12 @@ export default async (req, res, next) => {
 
   const [, token] = auth.split(" ");
   try {
-    const payload = jwt.verify(`${token}`, `${process.env.APP_SECRET}`);
+    const payload = jwt.verify(
+      `${token}`,
+      `${process.env.APP_SECRET}`
+    ) as JwtPayload;
+
+    req.id = payload.id;
   } catch (err) {
     //tem token, mas ele está errado ou expirado
     return res
