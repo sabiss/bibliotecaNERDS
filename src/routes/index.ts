bfb1b4d2234bac48c961d2f4025459d17efbb72f
sabiss <sabrinabezsilva@gmail.com>
2023-09-30 19:49:22 -0300

    utilizando rotas de administrador

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import usuarios from "../routes/usuariosRouter";
+import administradores from "../routes/administradoresRouter";
 
 const routes = (app) => {
   //pega a instância do express que é o app
   app.use(
     //outras rotas que serão USE(u)das
     express.json(),
-    usuarios
+    usuarios,
+    administradores
   );
 };
 
