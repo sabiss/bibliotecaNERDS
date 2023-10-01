530b767437f33f31397e1ea1aaafd6325fe2719a
sabiss <sabrinabezsilva@gmail.com>
2023-10-01 16:08:18 -0300

    FIX: retirando atributo desnecessário

--- src/models/Responsavel.ts
+++ src/models/Responsavel.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const responsavelSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   idade: { type: String, required: true },
-  matricula: { type: String, required: true },
   cpf: { type: String, required: true },
+  email: { type: String, required: true },
   senha: { type: String, required: true },
 });
 
