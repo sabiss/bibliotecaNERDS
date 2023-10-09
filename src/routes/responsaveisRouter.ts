c9e8a686f3ba0a03c0c3fa726f2dc4394f1951cc
sabiss <sabrinabezsilva@gmail.com>
2023-10-08 21:01:18 -0300

    FEAT: adicionando checagem de papeis para evitar que usuários não "responsáveis" acessem a rota

--- src/routes/responsaveisRouter.ts
+++ src/routes/responsaveisRouter.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import responsaveisController from "../controllers/responsaveisController";
-
+import middleware from "../middleware/authMiddleware";
+import checkRole from "../middleware/checkRoules";
 const router = express.Router();
 
 router
+  .post("/responsavel/login", responsaveisController.logarNoSistema)
+  .use(middleware, checkRole(["resp"]))
   .get("/responsavel/listarLivros", responsaveisController.listarLivros)
   .post("/responsavel/cadastrarLivro", responsaveisController.cadastrarLivro)
   .post("/responsavel/emprestarLivro/:id",responsaveisController.realizarEmprestimoDeLivro)
-  .post("/responsavel/login", responsaveisController.logarNoSistema)
+  
   .delete("/responsavel/deletarLivro/:id", responsaveisController.deletarLivro)
 
 export default router;
