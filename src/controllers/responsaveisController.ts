713ae0509abf6f2dfa61ff3129e5565e28569cae
sabiss <sabrinabezsilva@gmail.com>
2023-10-12 16:19:41 -0300

    FEAT: métodos de listar e realizar emprestimos

--- src/controllers/responsaveisController.ts
+++ src/controllers/responsaveisController.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import livros from "../models/Livro";
 import responsaveis from "../models/Responsavel";
-import jwt from "jsonwebtoken";
 import emprestimos from "../models/Emprestimo";
+import jwt from "jsonwebtoken";
 
 class responsaveisController {
   static cadastrarLivro = async (req, res) => {
@@ -33,10 +33,52 @@ class responsaveisController {
       return res.status(400).send({ message: "iD do usuário inválido" });
     }
 
-    const quantidadeEmprestimosDesteLivro = emprestimos.where({
+    const livroASerEmprestado = await livros.findById(idLivro);
+    const emprestimosJaFeitos = await emprestimos.countDocuments({
       idLivro: idLivro,
     });
-    return res.send(quantidadeEmprestimosDesteLivro);
+    console.log(emprestimosJaFeitos);
+
+    if (!livroASerEmprestado) {
+      return res.status(404).send({ message: "Livro não encontrado" });
+    }
+
+    const emprestimoRepetido = await emprestimos.countDocuments({
+      idLivro: idLivro,
+      idUsuario: idUsuario,
+    });
+
+    if (emprestimoRepetido) {
+      return res
+        .status(500)
+        .send({ message: "Esse usuário ja pegou este livro emprestado" });
+    }
+    if (emprestimosJaFeitos < livroASerEmprestado.quantidade) {
+      //verifica se ainda há cópias disponíveis
+      const dataAtual = new Date();
+      const dia = dataAtual.getDate();
+      const mes = dataAtual.getMonth();
+      const ano = dataAtual.getFullYear();
+      const dataEmprestimo = `${dia}/${mes}/${ano}`;
+      const dataDevolucao = `${dia}/${mes + 1}/${ano}`;
+
+      const novoEmprestimo = new emprestimos({
+        idUsuario,
+        idLivro,
+        dataEmprestimo,
+        dataDevolucao,
+      });
+      try {
+        await novoEmprestimo.save();
+        return res
+          .status(201)
+          .send({ message: "Empréstimo realizado com sucesso!" });
+      } catch (err) {
+        return res
+          .status(500)
+          .send({ message: `Erro ao realizar emprésimo -  ${err}` });
+      }
+    }
   };
   static listarLivros = async (req, res) => {
     try {
@@ -50,6 +92,22 @@ class responsaveisController {
       return res
         .status(500)
         .send({ message: `Erro ao buscar livros para listagem - ${err}` });
+    }
+  };
+  static listarEmprestimos = async (req, res) => {
+    try {
+      const listaDeEmprestimos = await emprestimos
+        .find()
+        .populate("idUsuario")
+        .populate("idLivro");
+      if (listaDeEmprestimos.length == 0) {
+        res.status(200).send({ message: "Não há empréstimos realizados" });
+      }
+      return res.status(200).json(listaDeEmprestimos);
+    } catch (err) {
+      return res
+        .status(500)
+        .send({ message: `Erro ao listar empréstimo já feitos - ${err}` });
     }
   };
   static deletarLivro = async (req, res) => {
