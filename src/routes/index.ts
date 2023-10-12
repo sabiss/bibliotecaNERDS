870c93d3307c5dfe610f3d5159861ad7095a6bd5
sabiss <sabrinabezsilva@gmail.com>
2023-10-12 14:32:58 -0300

    FEAT; tornando login e checkRoles rotas em cascata

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,20 @@
 import express from "express";
+import checkRole from "../middlewares/checkRoules";
 import usuarios from "../routes/usuariosRouter";
 import administradores from "../routes/administradoresRouter";
 import responsaveis from "../routes/responsaveisRouter";
+import login from "../routes/login";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const routes = (app) => {
   //pega a instância do express que é o app
   app.use(
     //outras rotas que serão USE(u)das
     express.json(),
+    login,
+    authMiddleware,
     usuarios,
+    checkRole(["resp", "adm"]),
     administradores,
     responsaveis
   );
