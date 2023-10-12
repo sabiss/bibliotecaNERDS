28f0e28352d3d1b3ff3fd592980153ed03b3f7dc
sabiss <sabrinabezsilva@gmail.com>
2023-10-12 13:56:19 -0300

    FEAT: criando checkRoules - verificará se o usuário tem permissão para acessar o que quer

---
+++ src/middlewares/checkRoules.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+interface JwtPayload {
+  //faz a comando "req.idUserRequest = payload.id" funcionar
+  tipo: string;
+}
+
+const checkRole = (roles: String[] | String) => async (req, res, next) => {
+  const tipoDoUser = req.tipo;
+  console.log(tipoDoUser + " " + roles);
+
+  if (roles == "all") {
+    return next();
+  }
+  if (!roles.includes(tipoDoUser)) {
+    return res
+      .status(401)
+      .send({ message: "você não tem permissão para acessar essa rota" });
+  }
+  return next(); //deu tudo certo, pode continuar o processo
+};
+export default checkRole;
