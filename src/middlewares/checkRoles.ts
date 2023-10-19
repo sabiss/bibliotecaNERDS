b10836886e602047068b7f9473ab4f0b8d44ad16
sabiss <sabrinabezsilva@gmail.com>
2023-10-19 16:46:06 -0300

    FIX: corrigindo erro ortográfico no nome do arquivo

--- src/middlewares/checkRoles.ts
+++ src/middlewares/checkRoles.ts
@@ -1,16 +1,6 @@
-import jwt from "jsonwebtoken";
-
-interface JwtPayload {
-  //faz a comando "req.idUserRequest = payload.id" funcionar
-  tipo: string;
-}
-
 const checkRole = (roles: String[] | String) => (req, res, next) => {
   const tipoDoUser = req.tipo;
 
-  if (roles == "all") {
-    return next();
-  }
   if (!roles.includes(tipoDoUser)) {
     return res
       .status(401)
