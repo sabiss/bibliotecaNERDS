bff79d2dbb76de21f86ba052624ccbf837b51edc
sabiss <sabrinabezsilva@gmail.com>
2023-10-22 09:43:14 -0300

    FEAT: criando corpo do site

--- front-end/Administrador/home/home.js
+++ front-end/Administrador/home/home.js
@@ -1,3 +1,17 @@
+// document.addEventListener("DOMContentLoaded", minhaFuncao);
+// function minhaFuncao() {
+//   const token = localStorage.getItem("token");
+
+//   const payload = JSON.parse(atob(token.split(".")[1]));
+
+//   if (payload != "adm") {
+//     window.location.assign("../../login.html");
+//   }
+// }
+
+// Adicione um ouvinte de evento para o evento 'DOMContentLoaded'
+
+//para o funcionamente da lista de opções ao clicar no avatar
 const avatarImg = document.getElementById("avatar-img");
 const avatarOptions = document.getElementById("avatar-options");
 
