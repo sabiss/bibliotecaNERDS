02a2adb768169164772694e144e3f62b6a6248c1
sabiss <sabrinabezsilva@gmail.com>
2023-10-23 14:06:35 -0300

    FEAT: entidade cópias

---
+++ src/models/Copia.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+import ferramentas from "../funcoesAuxiliares/ferramentas";
+
+const copiaSchema = new mongoose.Schema({
+  idLivro: { type: mongoose.Schema.ObjectId, ref: "livros", required: true },
+  codigoDeIdentificacao: { type: String },
+  emprestado: { type: Boolean, default: false },
+});
+
+copiaSchema.pre("save", async function (next) {
+  this.codigoDeIdentificacao = await ferramentas.gerarCodigoParaCopias();
+  next();
+});
+
+const copias = mongoose.model("Copias", copiaSchema);
+
+export default copias;
