a81efb6d8a5358e79f0ee71c39cddd209f6a108c
sabiss <sabrinabezsilva@gmail.com>
2023-11-30 20:02:31 -0300

    FEAT: funções para exibir possíveis livros que se queira criar uma cópia

---
+++ front-end/Administrador/adicionarNovaCópia/main.js
@@ -0,0 +1,61 @@
+const token = localStorage.getItem("token");
+
+document.addEventListener("DOMContentLoaded", verificaUsuario());
+function verificaUsuario() {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      window.location.assign("../../login.html");
+    }
+    const dataAtual = new Date();
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    const dataExpiracaoToken = new Date(payload.exp * 1000);
+  
+    if (payload.tipo != "adm" || dataExpiracaoToken < dataAtual) {
+      window.location.assign("../../login.html");
+    }
+}
+
+const sugestoesDiv = document.getElementById('sugestoes');
+const barraPesquisa = document.getElementById('barraPesquisa');
+
+function exibirSugestoes(sugestoes) {
+    sugestoesDiv.innerHTML = '';
+
+    if (sugestoes.length > 0) {
+        for(let livro of sugestoes){
+            sugestoesDiv.innerHTML+=
+            `<li onclick="selecionarSugestao('${livro.titulo}')">${livro.titulo} - ${livro.autor}</li>`
+        }
+
+        sugestoesDiv.style.display = 'block';
+    } else {
+        sugestoesDiv.style.display = 'none';
+    }
+}
+async function buscarSugestoes() {
+    const termoPesquisa = barraPesquisa.value;
+    if(termoPesquisa != ""){
+        try{
+            const respostaApi = await fetch(`http://localhost:3000/buscarLivros?palavra=${termoPesquisa}`, {
+                headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+                }})
+            console.log(respostaApi)
+            const sugestoes = await respostaApi.json()
+            console.log(sugestoes)
+            exibirSugestoes(sugestoes);
+        }catch(err){
+            console.error(err.erro)
+            alert(err.message)
+        }
+    }else{
+        const sugestoesDiv = document.getElementById('sugestoes');
+        sugestoesDiv.innerHTML = ""
+    }
+    
+}
+function selecionarSugestao(sugestao) {
+    barraPesquisa.value = sugestao;
+    sugestoesDiv.style.display = 'none';
+}
