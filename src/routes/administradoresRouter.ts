49a087c5fbf999c4a98eea5301031d119f4056d5
sabiss <sabrinabezsilva@gmail.com>
2023-12-03 11:27:15 -0300

    FIX: organizando rotas

--- src/routes/administradoresRouter.ts
+++ src/routes/administradoresRouter.ts
@@ -5,12 +5,14 @@ import checkRole from "../middlewares/checkRoles";
 const router = express.Router();
 
 router
-  .post("/cadastroAdm", checkRole(["adm"]), administradoresController.cadastrarAdministrador)
   .get("/listarUsers",checkRole(["adm"]),administradoresController.listarUsuarios)
   .get("/listarAdministradores", checkRole(["adm"]), administradoresController.listarAdministradores)
   .get("/listarResponsaveis", checkRole(["adm"]),administradoresController.listarResponsaveis)
+
+  .post("/cadastroAdm", checkRole(["adm"]), administradoresController.cadastrarAdministrador)
   .post("/cadastroUser", checkRole(["adm"]),administradoresController.cadastrarUsuario)
   .post("/cadastrarResponsavel",checkRole(["adm"]),administradoresController.cadastraResponsavel)
+  
   .delete("/deletarUser/:id", checkRole(["adm"]),administradoresController.deletarUsuario)
   .delete("/deletarResponsavel/:id", checkRole(["adm"]),administradoresController.deletarResponsavel)
 
