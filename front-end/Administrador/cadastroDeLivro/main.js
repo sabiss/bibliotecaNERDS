11e6ebea4b9b802edb2c58d1389dadef63672d69
sabiss <sabrinabezsilva@gmail.com>
2023-12-05 14:10:02 -0300

    FIX: configurando os alertas de erro, atenção e sucesso

--- front-end/Administrador/cadastroDeLivro/main.js
+++ front-end/Administrador/cadastroDeLivro/main.js
@@ -19,34 +19,34 @@ function verificaUsuario() {
   }
 }
 function geraAlerta(texto, tipoAlerta) {
-  const alerta = document.querySelector("div#alerta")
-  let text = document.querySelector('strong#textoDoAlerta')
-  switch(tipoAlerta){
-    case 'Erro':
-      alerta.classList.remove('alert-success')
-      alerta.classList.remove('alert-warning')
-      alerta.classList.add('alert-danger')
-        break
-    case 'AnuncioDeSucesso':
-      alerta.classList.remove('alert-danger')
-      alerta.classList.remove('alert-warning')
-      alerta.classList.add('alert-success')
-      break
-    case 'Atencao': 
-      alerta.classList.remove('alert-danger')
-      alerta.classList.remove('alert-success')
-      alerta.classList.add('alert-warning')
-      break
+  const alerta = document.querySelector("div#alerta");
+  let text = document.querySelector("strong#textoDoAlerta");
+  switch (tipoAlerta) {
+    case "Erro":
+      alerta.classList.remove("alert-success");
+      alerta.classList.remove("alert-warning");
+      alerta.classList.add("alert-danger");
+      break;
+    case "AnuncioDeSucesso":
+      alerta.classList.remove("alert-danger");
+      alerta.classList.remove("alert-warning");
+      alerta.classList.add("alert-success");
+      break;
+    case "Atencao":
+      alerta.classList.remove("alert-danger");
+      alerta.classList.remove("alert-success");
+      alerta.classList.add("alert-warning");
+      break;
   }
-  alerta.classList.remove('d-none')
-  alerta.classList.add('d-flex')
-  
+  alerta.classList.remove("d-none");
+  alerta.classList.add("d-flex");
+
   text.innerHTML = `${texto}`;
 }
-function fecharAlert(){
+function fecharAlert() {
   const alerta = document.querySelector("div#alerta");
-  alerta.classList.add('d-none')
-  alerta.classList.remove('d-flex')
+  alerta.classList.add("d-none");
+  alerta.classList.remove("d-flex");
 }
 function formatarTexto(input) {
   const textoDigitado = input.value;
@@ -71,63 +71,62 @@ async function cadastrarNovoLivro() {
 
   if (!titulo || !autor || !isbn || !numeroPaginas || !genero) {
     geraAlerta("Preencha todos os campos", "Atencao");
-  }else{
+  } else {
     const token = localStorage.getItem("token");
     const livro = {
       titulo: titulo,
       autor: autor,
       isbn: isbn,
       numero_paginas: numeroPaginas,
-      genero: genero
-    }
+      genero: genero,
+    };
     try {
       const retornoApi = await fetch(`${baseUrl}/cadastrarLivro`, {
-        method: 'POST',
+        method: "POST",
         mode: "cors",
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify(livro)
-      })
+        body: JSON.stringify(livro),
+      });
       const resposta = await retornoApi.json();
-      if(!retornoApi.ok){
-        geraAlerta(resposta.message, 'Erro');
-      }else{
-        await criarCopia(titulo)
+      if (!retornoApi.ok) {
+        geraAlerta(resposta.message, "Erro");
+      } else {
+        await criarCopia(titulo);
       }
-      
-      
     } catch (err) {
-      console.error(err.erro)
+      console.error(err.erro);
       geraAlerta(err.message, "Erro");
     }
   }
-
-  
 }
-async function criarCopia(titulo){
+async function criarCopia(titulo) {
   const token = localStorage.getItem("token");
 
-  try{
+  try {
     const respostaApi = await fetch(`http://localhost:3000/adicionarCopia`, {
-      method: 'POST',
+      method: "POST",
       mode: "cors",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify({titulo: titulo})
-    })
-    if(!respostaApi.ok){
-      const mensagem = await respostaApi.json()
-      console.error(mensagem.erro)
-      geraAlerta(mensagem.message, "Erro")
+      body: JSON.stringify({ titulo: titulo }),
+    });
+    if (!respostaApi.ok) {
+      const mensagem = await respostaApi.json();
+      console.error(mensagem.erro);
+      geraAlerta(mensagem.message, "Erro");
     }
-    const numeroDaCopia = await respostaApi.json()
-    geraAlerta(`O número de identificação do livro é: ${numeroDaCopia.numero}`, 'AnuncioDeSucesso')
-  }catch(err){
-    console.error(err.erro)
-    geraAlerta(err.message, "Erro")
+    const numeroDaCopia = await respostaApi.json();
+    geraAlerta(
+      `O número de identificação do livro é: ${numeroDaCopia.numero}`,
+      "AnuncioDeSucesso"
+    );
+  } catch (err) {
+    console.error(err.erro);
+    geraAlerta(err.message, "Erro");
   }
 }
