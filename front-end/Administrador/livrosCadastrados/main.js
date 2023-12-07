14a988a3a5cc799288da58ca40f271a25d447d36
sabiss <sabrinabezsilva@gmail.com>
2023-12-06 21:18:50 -0300

    FEAT: busca , edição e exclução de livros

--- front-end/Administrador/livrosCadastrados/main.js
+++ front-end/Administrador/livrosCadastrados/main.js
@@ -3,7 +3,7 @@ const token = localStorage.getItem("token");
 
 async function paginaCarregou() {
   verificarToken();
-  await criarCards();
+  await criarTr();
 }
 async function verificarToken() {
   const token = localStorage.getItem("token");
@@ -21,19 +21,54 @@ async function verificarToken() {
     window.location.assign("../../login.html");
   }
 }
-function geraErro(texto) {
-  const alert = document.querySelector("div#anuncioDeErro");
-  alert.classList.remove("d-none");
-  alert.classList.add("d-flex");
-  const text = document.querySelector("strong#textoDoErro");
+function geraAlerta(texto, tipoAlerta) {
+  const alerta = document.querySelector("div#alerta");
+  let text = document.querySelector("strong#textoDoAlerta");
+  switch (tipoAlerta) {
+    case "Erro":
+      alerta.classList.remove("alert-success");
+      alerta.classList.remove("alert-warning");
+      alerta.classList.add("alert-danger");
+      break;
+    case "AnuncioDeSucesso":
+      alerta.classList.remove("alert-danger");
+      alerta.classList.remove("alert-warning");
+      alerta.classList.add("alert-success");
+      break;
+    case "Atencao":
+      alerta.classList.remove("alert-danger");
+      alerta.classList.remove("alert-success");
+      alerta.classList.add("alert-warning");
+      break;
+  }
+  alerta.classList.remove("d-none");
+  alerta.classList.add("d-flex");
+
   text.innerHTML = `${texto}`;
 }
-function fecharAlert() {
-  const alert = document.querySelector("div#anuncioDeErro");
-  alert.classList.add("d-none");
-  alert.classList.remove("d-flex");
+function fecharAlerta() {
+  const alerta = document.querySelector("div#alerta");
+  alerta.classList.add("d-none");
+  alerta.classList.remove("d-flex");
 }
-async function criarCards() {
+async function getLivros() {
+  try {
+    const respostaApi = await fetch("http://localhost:3000/listarLivros", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const livros = await respostaApi.json();
+    return livros;
+  } catch (err) {
+    console.error(err.erro);
+    geraAlerta(err.message, "Erro");
+  }
+}
+async function criarTr() {
   const tbody = document.querySelector("tbody.listaLivros");
   tbody.innerHTML = "";
   let livros = await getLivros();
@@ -57,7 +92,7 @@ async function criarCards() {
                     <td>${livro.autor}</td>
                     <td>${numeroCopias.length}</td>
                     <td>${livro.numero_paginas}</td>
-                    <td onclick="editar('${livro._id}')"><div class="editar iconesAcao"><img src="../../assets/lapisEdicao.png"></div></td>
+                    <td onclick="preencherInputsParaEdicao('${livro._id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"><div class="editar iconesAcao"><img src="../../assets/lapisEdicao.png"></div></td>
                     <td onclick= "remover('${livro._id}')"><div class = "iconesAcao remover"><img src="../../assets/lixo.png"></div></td>
                 </tr>
             `;
@@ -79,32 +114,140 @@ async function exibirSugestoes(livros) {
   }
 
   for (let livro of livros) {
+    const numeroCopias = await getCopiasDeUmLivro(livro._id);
     tableRow.innerHTML += `
-        <tr>
-            <td>${livro.titulo}</td>
-            <td>${livro.autor}</td>
-            <td>${numeroCopias.length}</td>
-            <td>${livro.numero_paginas}</td>
-            <td onclick="editar('${livro._id}')"><div class="editar iconesAcao"><img src="../../assets/lapisEdicao.png"></div></td>
-            <td onclick= "remover('${livro._id}')"><div class = "iconesAcao remover"><img src="../../assets/lixo.png"></div></td>
-        `;
+      <tr>
+        <td>${livro.titulo}</td>
+        <td>${livro.autor}</td>
+        <td>${numeroCopias.length}</td>
+        <td>${livro.numero_paginas}</td>
+        <td onclick="preencherInputsParaEdicao('${livro._id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"><div class="editar iconesAcao"><img src="../../assets/lapisEdicao.png"></div></td>
+        <td onclick= "remover('${livro._id}')"><div class = "iconesAcao remover"><img src="../../assets/lixo.png"></div></td>
+      </tr> 
+    `;
   }
 }
-async function getLivros() {
+async function buscarLivro() {
+  const palavraNaBarraDePesquisa = document.querySelector(
+    "input#barraDePesquisa"
+  ).value;
+  if (palavraNaBarraDePesquisa === "") {
+    await criarTr();
+  } else {
+    const livros = await getLivros();
+    const livrosSemelhantes = buscarPalavraNosLivros(
+      livros,
+      palavraNaBarraDePesquisa
+    );
+    exibirSugestoes(livrosSemelhantes);
+  }
+}
+function removerAcentos(str) {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+function buscarPalavraNosLivros(arrayDeObjetos, palavra) {
+  const regexComAcento = new RegExp(`.*${palavra}.*`, "i");
+  const regexSemAcento = new RegExp(`.*${removerAcentos(palavra)}.*`, "i");
+
+  const objetosEncontrados = arrayDeObjetos.filter((objeto) => {
+    return (
+      objeto &&
+      typeof objeto === "object" &&
+      Object.values(objeto).some((valor) => {
+        return (
+          regexComAcento.test(String(valor)) ||
+          regexSemAcento.test(removerAcentos(String(valor)))
+        );
+      })
+    );
+  });
+
+  return objetosEncontrados;
+}
+let idLivroParaAtualizar;
+async function preencherInputsParaEdicao(idLivro) {
+  idLivroParaAtualizar = idLivro;
+  const titulo = document.querySelector("#novoTitulo");
+  const autor = document.querySelector("#novoAutor");
+  const isbn = document.querySelector("#novoISBN");
+  const numeroPaginas = document.querySelector("#novoNumeroDePaginas");
+  const genero = document.querySelector("#novoGenero");
+
   try {
-    const respostaApi = await fetch("http://localhost:3000/listarLivros", {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    const livros = await respostaApi.json();
-    return livros;
+    const respostaApi = await fetch(
+      `http://localhost:3000/listarUmLivro/${idLivro}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (respostaApi.ok) {
+      const dadosDoLivro = await respostaApi.json();
+      console.log(dadosDoLivro);
+      titulo.value = dadosDoLivro.titulo;
+      autor.value = dadosDoLivro.autor;
+      isbn.value = dadosDoLivro.isbn;
+      numeroPaginas.value = dadosDoLivro.numero_paginas;
+      genero.value = dadosDoLivro.genero;
+    }
   } catch (err) {
     console.error(err.erro);
-    geraErro(err.message);
+    geraAlerta(err.message, "Erro");
+  }
+}
+function formatarPalavras(palavra) {
+  const palavras = palavra.split(" ");
+
+  for (let i = 0; i < palavras.length; i++) {
+    palavras[i] = palavras[i][0].toUpperCase() + palavras[i].substr(1);
+  }
+
+  return palavras.join(" ");
+}
+async function atualizarLivro() {
+  const novoTitulo = formatarPalavras(
+    document.querySelector("#novoTitulo").value
+  );
+  const novoAutor = formatarPalavras(
+    document.querySelector("#novoAutor").value
+  );
+  const novoISBN = document.querySelector("#novoISBN").value;
+  const novoNumeroDePaginas = document.querySelector(
+    "#novoNumeroDePaginas"
+  ).value;
+  const novoGenero = document.querySelector("#novoGenero").value;
+
+  try {
+    const respostaApi = await fetch(
+      `http://localhost:3000/atualizarLivro/${idLivroParaAtualizar}`,
+      {
+        method: "PUT",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          novoTitulo,
+          novoAutor,
+          novoISBN,
+          novoNumeroDePaginas,
+          novoGenero,
+        }),
+      }
+    );
+    const mensagem = await respostaApi.json();
+    if (!respostaApi.ok) {
+      console.error(mensagem.erro);
+      geraAlerta(mensagem.message, "Erro");
+    } else {
+      geraAlerta(mensagem.message, "AnuncioDeSucesso");
+      await criarTr();
+    }
+  } catch (err) {
+    geraAlerta("Erro ao atualizar livro", "Erro");
   }
 }
 async function getCopiasDeUmLivro(idLivro) {
@@ -122,51 +265,14 @@ async function getCopiasDeUmLivro(idLivro) {
     );
     if (!respostaApi.ok) {
       const mensagem = await respostaApi.json();
-      console.erro(mensagem.erro);
-      geraErro(mensagem.message);
+      console.error(mensagem.erro);
+      geraAlerta(mensagem.message, "Erro");
     }
     const copias = await respostaApi.json();
     return copias;
   } catch (err) {
     console.error(err.erro);
-    geraErro(err.message);
-  }
-}
-async function buscarLivros() {
-  const filtro = document.querySelector("input#palavra").value;
-  if (filtro === "") {
-    await criarCards();
-  }
-  try {
-    const respostaApi = await fetch(
-      `http://localhost:3000/buscarLivros?palavra=${filtro}`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    if (!respostaApi.ok) {
-      const mensagem = await respostaApi.json();
-      console.error(mensagem.erro);
-      geraErro(mensagem.message);
-    }
-    const livros = await respostaApi.json();
-    const livrosComQuantidadeDeCopias = [];
-
-    for (let livro of livros) {
-      const copias = await getCopiasDeUmLivro(livro._id);
-      livrosComQuantidadeDeCopias.push({
-        livro,
-        numeroDeCopias: copias.length,
-      });
-    }
-
-    exibirSugestoes(livrosComQuantidadeDeCopias);
-  } catch (err) {
-    geraErro(err.message);
-    console.error(err.erro);
+    geraAlerta(err.message, "Erro");
   }
 }
 async function getLivros() {
@@ -179,20 +285,20 @@ async function getLivros() {
     });
     if (!respostaApi.ok) {
       const mensagem = await respostaApi.json();
-      geraErro(mensagem.message);
+      geraAlerta(mensagem.message, "Erro");
     }
 
     const livros = await respostaApi.json();
     return livros;
   } catch (err) {
     console.error(err.erro);
-    geraErro(err.message);
+    geraAlerta(err.message, "Erro");
   }
 }
 async function remover(idLivro) {
   try {
     const respostaApi = await fetch(
-      `http://localhost/deletarLivro/${idLivro}`,
+      `http://localhost:3000/deletarLivro/${idLivro}`,
       {
         method: "DELETE",
         mode: "cors",
@@ -202,8 +308,17 @@ async function remover(idLivro) {
         },
       }
     );
+    if (respostaApi.ok) {
+      const mensagem = await respostaApi.json();
+      geraAlerta(mensagem.message, "AnuncioDeSucesso");
+      await criarTr();
+    }
   } catch (err) {
-    console.error(err.erro);
-    geraErro(err.message);
+    if (err.erro) {
+      console.error(err.erro);
+    } else {
+      console.error(err);
+    }
+    geraAlerta(err.message, "Erro");
   }
 }
