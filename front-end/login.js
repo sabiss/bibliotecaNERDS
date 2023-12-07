59df032055280d93dc1e69fc9e0a93af3b5c55c6
sabiss <sabrinabezsilva@gmail.com>
2023-12-07 14:10:56 -0300

    FEAT: usuário resp será redirecionado para sua própria home

--- front-end/login.js
+++ front-end/login.js
@@ -29,32 +29,32 @@ function verificaSeAindaTemToken() {
 }
 
 function geraAlerta(texto, tipoAlerta) {
-  const alerta = document.querySelector("div#alerta")
-  let text = document.querySelector('strong#textoDoAlerta')
-  switch(tipoAlerta){
-    case 'Erro':
-      alerta.classList.remove('alert-success')
-      alerta.classList.remove('alert-warning')
-      alerta.classList.add('alert-danger')
-        break
-    case 'Atencao': 
-      alerta.classList.remove('alert-danger')
-      alerta.classList.remove('alert-success')
-      alerta.classList.add('alert-warning')
-      break
+  const alerta = document.querySelector("div#alerta");
+  let text = document.querySelector("strong#textoDoAlerta");
+  switch (tipoAlerta) {
+    case "Erro":
+      alerta.classList.remove("alert-success");
+      alerta.classList.remove("alert-warning");
+      alerta.classList.add("alert-danger");
+      break;
+    case "Atencao":
+      alerta.classList.remove("alert-danger");
+      alerta.classList.remove("alert-success");
+      alerta.classList.add("alert-warning");
+      break;
   }
-  alerta.classList.remove('d-none')
-  alerta.classList.add('d-flex')
-  
+  alerta.classList.remove("d-none");
+  alerta.classList.add("d-flex");
+
   text.innerHTML = `${texto}`;
 }
-function fecharAlert(){
+function fecharAlert() {
   const alert = document.querySelector("div#alerta");
-  alert.classList.add('d-none')
-  alert.classList.remove('d-flex')
+  alert.classList.add("d-none");
+  alert.classList.remove("d-flex");
 }
-document.addEventListener('keyup', function(event) {
-  if (event.key === 'Enter') {
+document.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
     logar();
   }
 });
@@ -64,11 +64,11 @@ async function logar(event) {
   const loadingOverlay = document.querySelector(".loading-overlay");
   loadingOverlay.style.display = "block";
 
-  if(email === ""){
-    geraAlerta("Digite seu Email", "Atencao")
-  }else if(senha === ""){
-    geraAlerta("Digite sua senha", "Atencao")
-  }else{
+  if (email === "") {
+    geraAlerta("Digite seu Email", "Atencao");
+  } else if (senha === "") {
+    geraAlerta("Digite sua senha", "Atencao");
+  } else {
     try {
       const retornoDaApi = await fetch("http://localhost:3000/login", {
         method: "POST",
@@ -77,34 +77,34 @@ async function logar(event) {
         body: JSON.stringify({ email, senha }),
       });
       const resposta = await retornoDaApi.json();
-  
-      if(!retornoDaApi.ok){
-        geraAlerta(resposta.message, "Erro")
-      }else{
-          const token = resposta.token;
-          localStorage.setItem("token", token);
-  
-          const payload = JSON.parse(atob(decodeURIComponent(token.split(".")[1])));//atob() decodifica base64 para sua sequencia normal, ou seja, o payload[1] será descriptografado sem precisar do jwt.verify
-      
-          const tipoDeUsuario = payload.tipo;
-      
-          switch (tipoDeUsuario) {
-            case "adm":
-              window.location.assign("./Administrador/home/index.html");
-              break;
-            case "resp":
-              window.location.assign("https://www.facebook.com");
-              break;
-            default:
-              window.location.assign("https://www.youtuber.com");
-              break;
-          }
+
+      if (!retornoDaApi.ok) {
+        geraAlerta(resposta.message, "Erro");
+      } else {
+        const token = resposta.token;
+        localStorage.setItem("token", token);
+
+        const payload = JSON.parse(
+          atob(decodeURIComponent(token.split(".")[1]))
+        ); //atob() decodifica base64 para sua sequencia normal, ou seja, o payload[1] será descriptografado sem precisar do jwt.verify
+
+        const tipoDeUsuario = payload.tipo;
+
+        switch (tipoDeUsuario) {
+          case "adm":
+            window.location.assign("./Administrador/home/index.html");
+            break;
+          case "resp":
+            window.location.assign("./Responsável/home/index.html");
+            break;
+          default:
+            geraAlerta("Você não possui cadastro no sistema", "Erro");
+            break;
+        }
       }
-      
     } catch (err) {
-      console.error(err.erro)
+      console.error(err.erro);
       geraAlerta(err.message, "Erro");
     }
   }
-  
 }
