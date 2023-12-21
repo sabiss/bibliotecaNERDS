d1f3b5a81f3f1f0f697a33c9a77dd4fc8fe24f9b
sabiss <sabrinabezsilva@gmail.com>
2023-12-21 15:59:01 -0300

    FEAT: adm agora tem foto de perfil

--- src/models/Administrador.ts
+++ src/models/Administrador.ts
@@ -6,7 +6,11 @@ const schemaAdm = new Schema({
   senha: { type: String, required: true },
   tipo: { type: String, required: true },
   cpf: { type: String, required: true },
-  fotoPerfil: { type: mongoose.Schema.Types.ObjectId, ref: "fotos" },
+  fotoPerfil: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "fotos",
+    required: false,
+  },
 });
 
 const administradores = mongoose.model("administradores", schemaAdm);
